39a6df17f2057dbdde26a7806c2eb176c7accd49
Gabriel Mota <gabriel.mota70@outlook.com>
2026-06-05 15:31:21 -0400

    Update Exercício1.c

--- Lista_Exercicios_1_Ponteiros/Exercício1.c
+++ Lista_Exercicios_1_Ponteiros/Exercício1.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-int main(){
-	
-	int num, *pont;
-	
-	pont = &num;
-	
-	printf("Digite o valor da variavel: %d\n", num);
-	printf("Endereco da variavel: %p\n", &num);
-	printf("Valor do pontero: %p\n", pont);
-	printf("Valor apontado pelo ponteiro: %d\n ", *pont);
-	
-	return 0;
+void trocar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Digite dois valores: ");
+    scanf("%d %d", &num1, &num2);
+
+    trocar(&num1, &num2);
+
+    printf("Valores trocados:\n");
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n", num2);
+
+    return 0;
 	
 }
